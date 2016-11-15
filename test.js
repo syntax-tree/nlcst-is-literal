38db1c2ec36f87a4f542b976bf64db2a20cac543
Titus Wormer <tituswormer@gmail.com>
2016-11-15 21:39:42 +0100

    Replace mocha with tape

--- test.js
+++ test.js
@@ -1,141 +1,142 @@
 'use strict';
 
-/* eslint-env mocha */
-
 var assert = require('assert');
+var test = require('tape');
 var visit = require('unist-util-visit');
 var retext = require('retext');
 var isLiteral = require('./');
 
-describe('isLiteral()', function () {
-  describe('should work', function () {
-    it('should throw when not given a parent', function () {
-      assert.throws(function () {
-        isLiteral();
-      }, /Parent must be a node/);
+test('isLiteral()', function (t) {
+  t.throws(
+    function () {
+      isLiteral();
+    },
+    /Parent must be a node/,
+    'should throw without arguments'
+  );
 
-      assert.throws(function () {
-        isLiteral({});
-      }, /Parent must be a node/);
-    });
+  t.throws(
+    function () {
+      isLiteral({});
+    },
+    /Parent must be a node/,
+    'should throw without parent'
+  );
 
-    it('should throw when not given an index', function () {
-      assert.throws(function () {
-        isLiteral({children: []});
-      }, /Index must be a number/);
-    });
+  t.throws(
+    function () {
+      isLiteral({children: []});
+    },
+    /Index must be a number/,
+    'should throw without index'
+  );
 
-    it('should work on single word sentences', function () {
-      assert.doesNotThrow(function () {
-        process('Well? Ha! Funky', function (node, index, parent) {
-          assert.strictEqual(isLiteral(parent, index), false);
-        });
+  t.doesNotThrow(
+    function () {
+      process('Well? Ha! Funky', function (node, index, parent) {
+        assert.strictEqual(isLiteral(parent, index), false);
       });
-    });
-  });
+    },
+    'should work on single word sentences'
+  );
 
-  describe('Initial', function () {
-    [
-      'Foo - is meant as a literal.',
-      'Foo – is meant as a literal.',
-      'Foo — is meant as a literal.',
-      'Foo– is meant as a literal.',
-      'Foo— is meant as a literal.',
-      'Foo–is meant as a literal.',
-      'Foo—is meant as a literal.',
-      'Foo: is meant as a literal.',
-      'Foo; is meant as a literal.'
-    ].forEach(function (fixture) {
-      it('should detect `' + fixture + '`', function () {
+  t.doesNotThrow(
+    function () {
+      [
+        'Foo - is meant as a literal.',
+        'Foo – is meant as a literal.',
+        'Foo — is meant as a literal.',
+        'Foo– is meant as a literal.',
+        'Foo— is meant as a literal.',
+        'Foo–is meant as a literal.',
+        'Foo—is meant as a literal.',
+        'Foo: is meant as a literal.',
+        'Foo; is meant as a literal.'
+      ].forEach(function (fixture) {
         process(fixture, function (node, index, parent) {
-          assert.strictEqual(isLiteral(parent, index), index === 0);
+          assert.strictEqual(isLiteral(parent, index), index === 0, fixture);
         });
       });
-    });
-  });
+    },
+    'Initial'
+  );
 
-  describe('Final', function () {
-    [
-      'Meant as a literal is - foo.',
-      'Meant as a literal is – foo.',
-      'Meant as a literal is — foo.',
-      'Meant as a literal is –foo.',
-      'Meant as a literal is —foo.',
-      'Meant as a literal is–foo.',
-      'Meant as a literal is—foo.',
-      'Meant as a literal is: foo.',
-      'Meant as a literal is; foo.'
-    ].forEach(function (fixture) {
-      it('should detect `' + fixture + '`', function () {
+  t.doesNotThrow(
+    function () {
+      [
+        'Meant as a literal is - foo.',
+        'Meant as a literal is – foo.',
+        'Meant as a literal is — foo.',
+        'Meant as a literal is –foo.',
+        'Meant as a literal is —foo.',
+        'Meant as a literal is–foo.',
+        'Meant as a literal is—foo.',
+        'Meant as a literal is: foo.',
+        'Meant as a literal is; foo.'
+      ].forEach(function (fixture) {
         process(fixture, function (node, index, parent) {
-          var seconLast = parent.children.length - 2;
-          assert.strictEqual(isLiteral(parent, index), index === seconLast);
+          assert.strictEqual(isLiteral(parent, index), index === parent.children.length - 2, fixture);
         });
       });
-    });
-  });
+    },
+    'Final'
+  );
 
-  describe('Internal', function () {
-    [
-      'The word, foo, is meant as a literal.',
-      'The word -foo- is meant as a literal.',
-      'The word –foo– is meant as a literal.',
-      'The word —foo— is meant as a literal.',
-      'The word - foo - is meant as a literal.',
-      'The word – foo – is meant as a literal.',
-      'The word — foo — is meant as a literal.',
-      'The word "foo" is meant as a literal.',
-      'The word \'foo\' is meant as a literal.',
-      'The word ‘foo’ is meant as a literal.',
-      'The word ‚foo’ is meant as a literal.',
-      'The word ’foo’ is meant as a literal.,',
-      'The word ‚foo’ is meant as a literal.',
-      'The word “foo” is meant as a literal.',
-      'The word ”foo” is meant as a literal.',
-      'The word „foo” is meant as a literal.',
-      'The word „foo“ is meant as a literal.',
-      'The word «foo» is meant as a literal.',
-      'The word »foo« is meant as a literal.',
-      'The word ‹foo› is meant as a literal.',
-      'The word ›foo‹ is meant as a literal.',
-      'The word (foo) is meant as a literal.',
-      'The word [foo] is meant as a literal.',
-      'The word {foo} is meant as a literal.',
-      'The word ⟨foo⟩ is meant as a literal.',
-      'The word 「foo」 is meant as a literal.'
-    ].forEach(function (fixture, n) {
-      var pos = 5;
-
-      /* Adjacent hyphens are part of the word. */
-      if (n === 1) {
-        pos = 4;
-      }
-
-      /* Tests for extra spaces. */
-      if (n === 4 || n === 5 || n === 6) {
-        pos = 6;
-      }
-
-      it('should detect `' + fixture + '`', function () {
+  t.doesNotThrow(
+    function () {
+      [
+        'The word, foo, is meant as a literal.',
+        'The word -foo- is meant as a literal.',
+        'The word –foo– is meant as a literal.',
+        'The word —foo— is meant as a literal.',
+        'The word - foo - is meant as a literal.',
+        'The word – foo – is meant as a literal.',
+        'The word — foo — is meant as a literal.',
+        'The word "foo" is meant as a literal.',
+        'The word \'foo\' is meant as a literal.',
+        'The word ‘foo’ is meant as a literal.',
+        'The word ‚foo’ is meant as a literal.',
+        'The word ’foo’ is meant as a literal.,',
+        'The word ‚foo’ is meant as a literal.',
+        'The word “foo” is meant as a literal.',
+        'The word ”foo” is meant as a literal.',
+        'The word „foo” is meant as a literal.',
+        'The word „foo“ is meant as a literal.',
+        'The word «foo» is meant as a literal.',
+        'The word »foo« is meant as a literal.',
+        'The word ‹foo› is meant as a literal.',
+        'The word ›foo‹ is meant as a literal.',
+        'The word (foo) is meant as a literal.',
+        'The word [foo] is meant as a literal.',
+        'The word {foo} is meant as a literal.',
+        'The word ⟨foo⟩ is meant as a literal.',
+        'The word 「foo」 is meant as a literal.'
+      ].forEach(function (fixture, n) {
         process(fixture, function (node, index, parent) {
-          assert.strictEqual(isLiteral(parent, index), index === pos);
+          var pos = 5;
+
+          /* Adjacent hyphens are part of the word. */
+          if (n === 1) {
+            pos = 4;
+          }
+
+          /* Tests for extra spaces. */
+          if (n === 4 || n === 5 || n === 6) {
+            pos = 6;
+          }
+
+          assert.strictEqual(isLiteral(parent, index), index === pos, fixture);
         });
       });
-    });
-  });
+    },
+    'Internal'
+  );
+
+  t.end();
 });
 
-/**
- * Shortcut to process a fixture and invoke `visitor`
- * for each of its word nodes.
- */
+/* Shortcut to process a fixture and invoke `visitor`
+ * for each of its word nodes. */
 function process(fixture, visitor) {
-  retext().process(fixture, function (err, file) {
-    /* istanbul ignore next */
-    if (err) {
-      throw err;
-    }
-
-    visit(file.namespace('retext').cst, 'WordNode', visitor);
-  });
+  visit(retext().parse(fixture), 'WordNode', visitor);
 }
