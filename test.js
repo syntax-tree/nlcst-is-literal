56868aa0d89ee1fdd06a0cee04a551b03acabe08
Titus Wormer <tituswormer@gmail.com>
2023-07-17 14:48:04 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -8,67 +8,61 @@ import test from 'node:test'
 import {ParseEnglish} from 'parse-english'
 import {visit} from 'unist-util-visit'
 import {isLiteral} from './index.js'
-import * as mod from './index.js'
 
-test('isLiteral()', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['isLiteral'],
-    'should expose the public api'
-  )
+test('isLiteral', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'isLiteral'
+    ])
+  })
 
-  assert.throws(
-    () => {
-      // @ts-expect-error runtime.
+  await t.test('should throw without arguments', async function () {
+    assert.throws(function () {
+      // @ts-expect-error: check how the runtime handles a missing node.
       isLiteral()
-    },
-    /Parent must be a node/,
-    'should throw without arguments'
-  )
+    }, /Parent must be a node/)
+  })
 
-  assert.throws(
-    () => {
-      // @ts-expect-error runtime.
+  await t.test('should throw without parent', async function () {
+    assert.throws(function () {
+      // @ts-expect-error: check how the runtime handles a non-none.
       isLiteral({})
-    },
-    /Parent must be a node/,
-    'should throw without parent'
-  )
+    }, /Parent must be a node/)
+  })
 
-  assert.throws(
-    () => {
-      // @ts-expect-error runtime.
+  await t.test('should throw without node', async function () {
+    assert.throws(function () {
+      // @ts-expect-error: check how the runtime handles a non-none.
       isLiteral({children: []})
-    },
-    /Index must be a number/,
-    'should throw without node'
-  )
+    }, /Index must be a number/)
+  })
 
-  assert.throws(
-    () => {
-      // @ts-expect-error runtime.
+  await t.test('should throw if `node` is not in `parent`', async function () {
+    assert.throws(function () {
+      // @ts-expect-error: check how the runtime handles a non-none.
       isLiteral({children: []}, {type: 'a'})
-    },
-    /Node must be a child of `parent`/,
-    'should throw if `node` is not in `parent`'
-  )
+    }, /Node must be a child of `parent`/)
+  })
 
-  assert.doesNotThrow(() => {
-    const n = {type: 'a'}
-    // @ts-expect-error runtime.
-    isLiteral({children: [n]}, n)
-  }, 'should not throw if `node` is in `parent`')
-
-  assert.doesNotThrow(() => {
-    process('Well? Ha! Funky', (_, index, parent) => {
-      assert.strictEqual(
-        parent && index !== undefined && isLiteral(parent, index),
-        false
-      )
+  await t.test('should not throw if `node` is in `parent`', async function () {
+    assert.doesNotThrow(function () {
+      const n = {type: 'a'}
+      // @ts-expect-error: check how the runtime handles a non-none.
+      isLiteral({children: [n]}, n)
     })
-  }, 'should work on single word sentences')
+  })
 
-  assert.doesNotThrow(() => {
+  await t.test('should work on single word sentences', async function () {
+    assert.doesNotThrow(function () {
+      process('Well? Ha! Funky', function (_, index, parent) {
+        assert(parent)
+        assert(index !== undefined)
+        assert.strictEqual(isLiteral(parent, index), false)
+      })
+    })
+  })
+
+  await t.test('should work on initial nodes', async function () {
     const fixtures = [
       'Foo - is meant as a literal.',
       'Foo – is meant as a literal.',
@@ -83,17 +77,15 @@ test('isLiteral()', () => {
     let index = -1
 
     while (++index < fixtures.length) {
-      process(fixtures[index], (_, index, parent) => {
-        assert.strictEqual(
-          parent && index !== undefined && isLiteral(parent, index),
-          index === 0,
-          String(index)
-        )
+      process(fixtures[index], function (_, index, parent) {
+        assert(parent)
+        assert(index !== undefined)
+        assert.strictEqual(isLiteral(parent, index), index === 0)
       })
     }
-  }, 'Initial')
+  })
 
-  assert.doesNotThrow(() => {
+  await t.test('should work on final nodes', async function () {
     const fixtures = [
       'Meant as a literal is - foo.',
       'Meant as a literal is – foo.',
@@ -108,17 +100,18 @@ test('isLiteral()', () => {
     let index = -1
 
     while (++index < fixtures.length) {
-      process(fixtures[index], (_, index, parent) => {
+      process(fixtures[index], function (_, index, parent) {
+        assert(parent)
+        assert(index !== undefined)
         assert.strictEqual(
-          parent && index !== undefined && isLiteral(parent, index),
-          parent && index === parent.children.length - 2,
-          String(index)
+          isLiteral(parent, index),
+          index === parent.children.length - 2
         )
       })
     }
-  }, 'Final')
+  })
 
-  assert.doesNotThrow(() => {
+  await t.test('should work on internal nodes', async function () {
     const fixtures = [
       'The word, foo, is meant as a literal.',
       'The word -foo- is meant as a literal.',
@@ -150,7 +143,7 @@ test('isLiteral()', () => {
     let index = -1
 
     while (++index < fixtures.length) {
-      process(fixtures[index], (_, place, parent) => {
+      process(fixtures[index], function (_, place, parent) {
         let pos = 5
 
         // Adjacent hyphens are part of the word.
@@ -163,21 +156,22 @@ test('isLiteral()', () => {
           pos = 6
         }
 
-        assert.strictEqual(
-          parent && place !== undefined && isLiteral(parent, place),
-          place === pos,
-          String(index)
-        )
+        assert(parent)
+        assert(place !== undefined)
+        assert.strictEqual(isLiteral(parent, place), place === pos)
       })
     }
-  }, 'Internal')
+  })
 })
 
 /**
  * Shortcut to process a fixture and invoke `visitor` for each of its word
  * nodes.
+ *
  * @param {string} fixture
+ *   Natural language.
  * @param {Visitor} visitor
+ *   Visitor.
  */
 function process(fixture, visitor) {
   const tree = new ParseEnglish().parse(fixture)
